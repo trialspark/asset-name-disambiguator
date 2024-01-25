f2afea4c11cf81c1f0f79b93535255d33910ff86
emilyw26 <emilywu1456@gmail.com>
2024-01-25 10:59:31 -0500

    Add compound-and-brands endpoint

--- app/pubchem_service.py
+++ app/pubchem_service.py
@@ -2,33 +2,65 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
+from time import sleep
+
+import requests
+import urllib.parse
 
 
 class PubchemService:
+    def __init__(self):
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        self.driver = webdriver.Chrome(options=options)
+
     def get_synonym(self, asset_name):
         try:
-            options = Options()
-            options.add_argument('--headless')
-            options.add_argument('--disable-gpu')
-            driver = webdriver.Chrome(options=options)
-            driver.get("https://pubchem.ncbi.nlm.nih.gov/")
-            search_bar = driver.find_element(By.TAG_NAME, 'input')
-            search_bar.click()
-            search_bar.send_keys(asset_name)
-            search_button = driver.find_element(By.CLASS_NAME, 'main-search-submit')
-            search_button.click()
-            driver.implicitly_wait(5)
-            result_link = driver.find_element(By.CLASS_NAME, 'highlight')
-            result_link.click()
-            driver.implicitly_wait(5)
-            synonym_section = driver.find_element(By.ID, 'Depositor-Supplied-Synonyms')
+            self.search_for_asset(asset_name)
+            synonym_section = self.driver.find_element(By.ID, 'Depositor-Supplied-Synonyms')
             synonyms = synonym_section.find_elements(By.TAG_NAME, "li")
             synonyms_list = []
             for item in synonyms:
                 synonyms_list.append(item.text)
-            driver.quit()
+            self.driver.quit()
             return synonyms_list
 
-        except NoSuchElementException as e:
+        except NoSuchElementException:
             print(f"Compound {asset_name} not found.")
             return []
+
+    def get_compound_and_brands(self, asset_name):
+        try:
+            self.search_for_asset(asset_name)
+            compound_name = self.driver.find_element(By.TAG_NAME, "h1").text
+            brand_names = self.get_fda_approved_drugs(compound_name)
+            return {'compound_name': compound_name, 'brand_names': list(brand_names)}
+        except NoSuchElementException:
+            print(f"Compound {asset_name} not found.")
+            return {'compound_name': None, 'brand_names': []}
+        pass
+
+    def get_fda_approved_drugs(self, compound_name):
+        response = requests.get(f'https://api.fda.gov/drug/drugsfda.json?search=products.active_ingredients.name:"{urllib.parse.quote_plus(compound_name)}"&limit=1000').json()
+        brand_names = set()
+        if 'results' not in response:
+            print(f'no brand names found for {compound_name}')
+            return []
+        for result in response['results']:
+            for product in result['products']:
+                brand_names.add(product['brand_name'])
+
+        return brand_names
+
+    def search_for_asset(self, asset_name):
+        self.driver.get("https://pubchem.ncbi.nlm.nih.gov/")
+        search_bar = self.driver.find_element(By.TAG_NAME, 'input')
+        search_bar.click()
+        search_bar.send_keys(asset_name)
+        search_button = self.driver.find_element(By.CLASS_NAME, 'main-search-submit')
+        search_button.click()
+        self.driver.implicitly_wait(5)
+        result_link = self.driver.find_element(By.XPATH, "//a[contains(@data-action,'featured-result-link')]")
+        result_link.click()
+        self.driver.implicitly_wait(5)
--- app/routes.py
+++ app/routes.py
@@ -4,10 +4,20 @@ from app import app
 
 from app.pubchem_service import PubchemService
 
+service = PubchemService()
+
 
 @app.route('/synonyms')
 def get_synonyms():
     asset = request.args.get('asset')
     if asset is None:
         return "please provide an asset url param e.g., \"synonyms?asset=aspirin\""
-    return PubchemService().get_synonym(asset)
+    return service.get_synonym(asset)
+
+
+@app.route('/compound-and-brands')
+def get_compound_and_brands():
+    asset = request.args.get('asset')
+    if asset is None:
+        return "please provide an asset url param e.g., \"compound-and-brands?asset=aspirin\""
+    return service.get_compound_and_brands(asset)
